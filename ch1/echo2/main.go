950d6baa770251d3901e7c99e807a467e7e839c3
Toby O'Sullivan <toby@justgiving.com>
2018-02-13 18:21:23 +0000

    Exercise 1.2b

---
+++ ch1/echo2/main.go
@@ -0,0 +1,18 @@
+// Uses range to iterate over arguments
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var s, sep string
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
