1a16188bd2bcc409ac3e2443cc458a54a64a0c54
Toby O'Sullivan <toby@justgiving.com>
2018-02-20 17:24:08 +0000

    Chapter 1.5 exercises

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -4,24 +4,29 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http") {
+			url = fmt.Sprintf("http://%s", url)
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		count, err := io.Copy(os.Stdout, resp.Body)
+		status := resp.Status
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		fmt.Printf("Retrieved %d bytes :%s", count, status)
 	}
 }
