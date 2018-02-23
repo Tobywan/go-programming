f8badadb2b4dd57be4267c89e2de2097520e80cc
Toby O'Sullivan <toby@justgiving.com>
2018-02-23 07:34:29 +0000

    http server with header parsing

---
+++ ch1/server3/main.go
@@ -0,0 +1,54 @@
+// Server2 is a minimal echo server
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+
+	port := os.Args[1]
+	if port == "" {
+		port = "8000"
+	}
+
+	hostport := fmt.Sprintf("localhost:%s", port)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe(hostport, nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Request count:%d\n", count)
+	mu.Unlock()
+}
