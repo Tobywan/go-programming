e9f0d3c24ec3a8b12d1067ee10b04107a0c5f667
tobywan <wxyz4321@hotmail.com>
2018-05-19 12:12:07 +0100

    Testing untyped consts)

--- ch3/iota/example.go
+++ ch3/iota/example.go
@@ -11,3 +11,18 @@ const (
 	ziB
 	yiB // 2 ^ 80
 )
+
+// KB = 10^3
+// YB = 10^24
+// How can we generate consts compactly?
+// given that 10^(3*iota) won't work
+const (
+	KB = 1000    // assumes next expression of 1 << ( 10) = 2^ 10 = 1024
+	MB = KB * KB // 2 ^ 20
+	GB = MB * KB
+	TB = GB * KB
+	PB = TB * KB
+	EB = PB * KB
+	ZB = EB * KB
+	YB = ZB * KB // 2 ^ 80
+)
---
+++ scratch/conv/convert_test.go
@@ -0,0 +1,43 @@
+package conv
+
+import (
+	"testing"
+)
+
+func TestCompile(t *testing.T) {
+	var i = 1
+	var i32 int32 = 2
+	var i64 int64 = 3
+
+	res := i + int(i32) + int(i64)
+
+	t.Logf("%d + %d + %d = %d", i, i32, i64, res)
+
+	x := 2
+	x32 := int32(4)
+	x64 := int64(8)
+	res = x + int(x32) + int(x64)
+
+	t.Logf("%d + %d + %d = %d", x, x32, x64, res)
+
+}
+
+func TestCast(t *testing.T) {
+	var f float64
+
+	f = 5 / 9
+	t.Logf("f=%f", f)
+
+	f = 5.0 / 9
+	t.Logf("f=%f", f)
+
+	f = 5 / 9.0
+	t.Logf("f=%f", f)
+
+	f = (5 + 9) / 3
+	t.Logf("f=%f", f)
+
+	f = 2 + 0i
+	t.Logf("f=%f", f)
+
+}
