a0940687ae3acc6589e39554d180315a7841effe
tobywan <wxyz4321@hotmail.com>
2018-05-22 17:07:11 +0100

    INvestigaste slices

--- scratch/slices/slice_test.go
+++ scratch/slices/slice_test.go
@@ -1,6 +1,7 @@
 package slices
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -32,4 +33,26 @@ func TestCreate(t *testing.T) {
 	t.Logf("b: %v, len: %d", b, len(b))
 	t.Logf("c: %v, len: %d cap: %d", c, len(c), cap(c))
 	t.Logf("d: %v, len: %d cap: %d", d, len(d), cap(d))
+}
+
+func TestAppend(t *testing.T) {
+	dst := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	src := []int{10, 11, 12}
+	grow := make([]int, len(dst)+len(src))
+
+	t.Logf(sniff(dst, "dst"))
+	t.Logf(sniff(src, "src"))
+	t.Logf(sniff(grow, "grow"))
+
+	n := copy(grow[:len(dst)], dst)
+	t.Logf("copied: %d", n)
+	t.Logf(sniff(grow, "grow"))
+
+	n = copy(grow[len(dst):], src)
+	t.Logf("copied: %d", n)
+	t.Logf(sniff(grow, "grow"))
+}
+
+func sniff(sl []int, name string) string {
+	return fmt.Sprintf("%s:%v len:%d cap:%d", name, sl, len(sl), cap(sl))
 }
